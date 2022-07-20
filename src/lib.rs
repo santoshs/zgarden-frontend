7baaf1744ac5e8d7089b9944c4ec0a732206f03e
ssivaraj <ssivaraj@nvidia.com>
2022-07-20 13:23:03 +0530

    Fix some url issues
    Signed-off-by: ssivaraj <ssivaraj@nvidia.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use std::panic;
+use url::{ParseError, Url};
 use wasm_bindgen::{prelude::*, JsCast};
 use wasm_bindgen_futures::spawn_local;
+use web_sys::Element;
 
 mod page;
 
@@ -26,23 +28,27 @@ pub fn log(s: &str) {
 }
 
 #[wasm_bindgen]
-pub fn page_init() {
+pub fn setup() {
     panic::set_hook(Box::new(console_error_panic_hook::hook));
 
     let window = web_sys::window().expect("Global window does not exist");
     let document = window.document().expect("Expecting a document on window");
-    let page = document.get_elements_by_class_name("page").item(0);
+    let page = document.get_element_by_id("main");
     match page {
         Some(p) => {
-            let links = p
-                .query_selector_all("a")
-                .unwrap()
-                .dyn_into::<web_sys::NodeList>()
-                .unwrap();
-            setup_links(links);
+            page_init(p);
         }
         None => log("Cannot get element with class page"),
     }
+}
+
+pub fn page_init(page: Element) {
+    let links = page
+        .query_selector_all("a")
+        .unwrap()
+        .dyn_into::<web_sys::NodeList>()
+        .unwrap();
+    setup_links(links);
 }
 
 pub fn setup_links(links: web_sys::NodeList) {
@@ -51,12 +57,31 @@ pub fn setup_links(links: web_sys::NodeList) {
     for i in 0..links.length() {
         let c = Closure::wrap(Box::new(move |e: web_sys::MouseEvent| {
             let target = e.target().unwrap().dyn_into::<web_sys::Element>().unwrap();
-            let url = target.get_attribute("href").unwrap();
+            let mut url = target.get_attribute("href").unwrap();
+            let window = web_sys::window().expect("Global window does not exist");
+
+            let location = window.location();
+            loop {
+                let parsed_url = Url::parse(&url);
+                match parsed_url {
+                    Ok(u) => {
+                        console_log(u.as_str());
+                        break;
+                    }
+                    Err(e) => match e {
+                        ParseError::RelativeUrlWithoutBase => {
+                            url = location.origin().unwrap() + &url;
+                        }
+                        _ => console_log(&format!("{:?}", e)),
+                    },
+                };
+            }
+
             e.prevent_default();
             e.stop_immediate_propagation();
             e.stop_propagation();
 
-            spawn_local(page::add_page("http:".to_string() + &url + "/index.json"));
+            spawn_local(page::add_page(url + "/index.json"));
         }) as Box<dyn FnMut(_)>);
 
         log("adding click event");
--- src/page.rs
+++ src/page.rs
@@ -1,5 +1,6 @@
-use crate::alert;
+use crate::{console_log, page_init};
 use reqwest::{self, header, header::CONTENT_TYPE};
+use serde::Deserialize;
 
 pub async fn add_page(url: String) {
     let mut headers = header::HeaderMap::new();
@@ -13,5 +14,35 @@ pub async fn add_page(url: String) {
         .build()
         .unwrap();
     let res = client.get(url).send().await.unwrap();
-    alert(&res.text().await.unwrap());
+    // alert(&res.text().await.unwrap());
+
+    let window = web_sys::window().expect("Global window does not exist");
+    let document = window.document().expect("Expecting a document on window");
+    let page = document.get_element_by_id("main");
+    match page {
+        Some(p) => {
+            let elem = document.create_element("div").unwrap();
+            elem.set_class_name("note");
+            elem.set_class_name("col-4");
+            p.append_child(&elem).unwrap();
+
+            #[derive(Deserialize)]
+            struct NoteNode {
+                title: String,
+                content: String,
+            }
+            let note = res.json::<NoteNode>().await.unwrap();
+            let title = document.create_element("h2").unwrap();
+            title.set_inner_html(&note.title);
+            elem.append_child(&title).unwrap();
+
+            let content = document.create_element("div").unwrap();
+            content.set_inner_html(&note.content);
+            elem.append_child(&content).unwrap();
+            //elem.set_inner_html(&note.content);
+
+            page_init(elem);
+        }
+        None => console_log("Cannot get main element"),
+    }
 }
